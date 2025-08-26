663256f31af15d535023a8ed6d463bf3abe9a6d9
jhamilellapacu-lang <jhamile.llapacu@ucb.edu.bo>
2025-08-26 11:48:14 -0400

    ejercicio_22.cpp

--- practica 1/ejercicio_22.cpp
+++ practica 1/ejercicio_22.cpp
@@ -2,13 +2,13 @@
 // Autor: Jhamile Llapacu Cruz
 // Carnet: 10935560 L.P.
 // Carrera del estudiante: Ingeniería de Sistemas
-// Fecha creación: 21/08/2025
+// Fecha creación: 26/08/2025
 // Número de ejercicio: 22
 #include<iostream>
 using namespace std;
 int main()
 {
-    int numero_1=0, numero_2=0, multiplo_1=0, multiplo_2=0;
+    int numero_1=0, numero_2=0;
     cout<<"Ingrese el primer numero: "<<endl;
     cin>>numero_1;
     cout<<"Ingrese el segundo numero: "<<endl;
