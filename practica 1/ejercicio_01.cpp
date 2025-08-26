a5abe84dd15dbd81d6a4d96c18d14e4f827318eb
jhamilellapacu-lang <jhamile.llapacu@ucb.edu.bo>
2025-08-25 23:47:11 -0400

    Add files via upload

---
+++ practica 1/ejercicio_01.cpp
@@ -0,0 +1,18 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Jhamile Llapacu Cruz
+// Carnet: 10935560 L.P.
+// Carrera del estudiante: Ingeniería de Sistemas
+// Fecha creación: 25/08/2025
+// Número de ejercicio: 1
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hola, soy estudiante de programación."<<endl;
+    cout<<"Mi nombre es Jhamile Llapacu Cruz."<<endl;
+    cout<<"Soy de Ingenieria de Sistemas";
+    return 0;
+}
+
+
