5839092534c3f8f41d78db3507d86e7af8d490d0
jhamilellapacu-lang <jhamile.llapacu@ucb.edu.bo>
2025-08-26 11:46:15 -0400

    Add files via upload
    Se realizo el ejercicio 22 con éxito.

---
+++ practica 1/ejercicio_22.cpp
@@ -0,0 +1,28 @@
+//Materia: Programación I, Paralelo 4
+// Autor: Jhamile Llapacu Cruz
+// Carnet: 10935560 L.P.
+// Carrera del estudiante: Ingeniería de Sistemas
+// Fecha creación: 21/08/2025
+// Número de ejercicio: 22
+#include<iostream>
+using namespace std;
+int main()
+{
+    int numero_1=0, numero_2=0, multiplo_1=0, multiplo_2=0;
+    cout<<"Ingrese el primer numero: "<<endl;
+    cin>>numero_1;
+    cout<<"Ingrese el segundo numero: "<<endl;
+    cin>>numero_2;
+    if(numero_1%numero_2==0){
+        cout<<"Son multiplos. "<<numero_1<<" es multiplo de "<<numero_2<<endl;
+    }
+       else if(numero_2%numero_1==0){
+           cout<<"Son multiplos. "<<numero_2<<" es multiplo de "<<numero_1<<endl;
+       }
+           
+            else{
+                cout<<"No son multiplos";
+         
+            }
+return 0;
+}
