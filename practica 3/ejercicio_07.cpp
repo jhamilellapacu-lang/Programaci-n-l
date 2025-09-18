e70a0890fa7f1ffec167ce3217cf772197a2fafb
jhamilellapacu-lang <jhamile.llapacu@ucb.edu.bo>
2025-09-18 16:21:58 -0400

    ejercicio_07.cpp

--- practica 3/ejercicio_07.cpp
+++ practica 3/ejercicio_07.cpp
@@ -1,20 +1,17 @@
-// Materia: Programación I, Paralelo 4
+// Materia: ProgramaciÃ³n I, Paralelo 4
 // Autor: Jhamile Llapacu Cruz.
-// Fecha creación: 10/09/2025
-// Número de ejercicio: 7
+// Fecha creaciÃ³n: 10/09/2025
+// NÃºmero de ejercicio: 7
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
 int generarEdad() {
     return rand() % 35 + 1;
 }
-
 int generarAltura() {
     return rand() % 81 + 120;
 }
-
 void analizarAlumnos(int numero) {
     int edad=0, altura=0, sumaEdades = 0, sumaAlturas = 0, mayores18 = 0, mas175 = 0;
     float edadMedia=0.0, alturaMedia=0.0;
@@ -24,36 +21,28 @@ void analizarAlumnos(int numero) {
         cout << "Alumno " << i << ": Edad = " << edad << ", Altura = " << altura << endl;
         sumaEdades += edad;
         sumaAlturas += altura;
-
         if (edad > 18) {
             mayores18 ++;
         }
-
         if (altura > 175) {
             mas175 ++;
         }
     }
-
     edadMedia = (float)sumaEdades / numero;
     alturaMedia = (float)sumaAlturas / numero;
-
     cout << "Edad media: " << edadMedia << " annos"<<endl;
     cout << "Altura media: " << alturaMedia << " cm"<<endl;
     cout << "Alumnos mayores de 18 annos: " << mayores18 << endl;
     cout << "Alumnos que miden mas de 175 cm: " << mas175 <<endl;
 }
 
-
 int main() {
     int numero;
-
     srand(time(0)); 
-
     cout << "Ingrese la cantidad de alumnos: ";
     cin >> numero;
-
     analizarAlumnos(numero);
-
     return 0;
 }
+
 
