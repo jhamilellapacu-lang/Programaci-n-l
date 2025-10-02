e5b8cf18f1e4824016fbb42bb1a53d82dda77e88
jhamilellapacu-lang <jhamile.llapacu@ucb.edu.bo>
2025-10-02 16:16:45 -0400

    ejercicio_05.cpp

--- practica 5.0/Ejercicio_05 (1).cpp
+++ practica 5.0/Ejercicio_05 (1).cpp
@@ -7,38 +7,31 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 int main() {
     int N;
     cout << "Ingrese la dimensión de los vectores: ";
     cin >> N;
-
     vector<int> A(N), B(N), C;
-
     cout << "Ingrese los elementos del vector A:\n";
     for (int i = 0; i < N; i++) {
         cout << "A[" << i << "]: ";
         cin >> A[i];
     }
-
     cout << "Ingrese los elementos del vector B:\n";
     for (int i = 0; i < N; i++) {
         cout << "B[" << i << "]: ";
         cin >> B[i];
     }
-
     for (int i = 0; i < N; i++) {
         C.push_back(A[i]);
     }
     for (int i = 0; i < N; i++) {
         C.push_back(B[i]);
     }
-
     cout << "Vector combinado C:\n";
     for (int i = 0; i < C.size(); i++) {
         cout << C[i] << " ";
     }
     cout << endl;
-
     return 0;
 }
