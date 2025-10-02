383e65b343407fbcf3a41c15194ecbde3c654a87
jhamilellapacu-lang <jhamile.llapacu@ucb.edu.bo>
2025-10-02 16:20:05 -0400

    ejercicio_03 (1).cpp

--- practica 5.0/Ejercicio_03 (1).cpp
+++ practica 5.0/Ejercicio_03 (1).cpp
@@ -8,43 +8,34 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-
 int main() {
     int N;
     cout << "Ingrese la cantidad de calificaciones: ";
     cin >> N;
-
     vector<int> calificaciones(N);
     vector<float> desviacion(N);
-
     float suma = 0;      
     float promedio, varianza, sumaCuadrados = 0;
-    
     for (int i = 0; i < N; i++) {
         cout << "Calificacion " << i + 1 << ": ";
         cin >> calificaciones[i];
         suma += calificaciones[i];  
     }
-
     promedio = suma / N;  
     for (int i = 0; i < N; i++) {
         desviacion[i] = calificaciones[i] - promedio;
     }
-
     cout << " Calificacion    Desviacion ";
     for (int i = 0; i < N; i++) {
         cout << calificaciones[i] << "\t\t" << desviacion[i] << endl;
     }
 
-
     for (int i = 0; i < N; i++) {
         sumaCuadrados += desviacion[i] * desviacion[i];
     }
     varianza = sumaCuadrados / N;
-
     cout << "Suma total: " << suma << endl;
     cout << "Promedio: " << promedio << endl;
     cout << "Varianza: " << varianza << endl;
-
     return 0;
 }
